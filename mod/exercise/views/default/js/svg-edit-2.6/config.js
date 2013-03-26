8cb1589d5e6176e3821d692d9df28b02ae789313
Simon Ebeling <se222ej@student.lnu.se>
2013-03-26 23:39:13 +0100

    26/3 Start av exercise-editorn

---
+++ mod/exercise/views/default/js/svg-edit-2.6/config.js
@@ -0,0 +1,11 @@
+svgEditor.setConfig({
+        dimensions: [600, 1040],
+        canvas_expansion: 3,
+        initFill: {
+                color: '0000FF'
+        },
+		bkgd_color: {
+                color: '8ccb5a'
+        },
+		extensions: ['ext-arrows.js','ext-connector.js']
+});
